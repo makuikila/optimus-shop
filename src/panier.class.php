de30a204f211e2936315cb7cfca0b372e1002351
makuikila <127111883+makuikila@users.noreply.github.com>
2026-06-19 13:31:03 +0300

    Add files via upload

--- src/panier.class.php
+++ src/panier.class.php
@@ -1,45 +1,40 @@
-<?php 
-    class panier {
-        private $DB;
+<?php
+class panier {
+    private $db;
 
-        public function __construct($DB){
-            if(!isset($_SESSION)){
-                session_start(); 
-            }
-            if(!isset($_SESSION['panier'])){
-                $_SESSION['panier'] = array();
-            }
-            $this->DB = $DB;
+    public function __construct($db) {
+        $this->db = $db;
+    }
+
+    // Add product to session cart
+    public function addPanier($id_produit) {
+        if (!isset($_SESSION['panier'])) {
+            $_SESSION['panier'] = [];
         }
-
-        public function total(){
-            $total = 0;
-            $ids = array_keys($_SESSION['panier']);
-            if(empty($ids)){
-                $rep = array();
-            }
-            else{
-                $rep = $this->DB->query('SELECT id_produit, prix FROM produit WHERE id_produit IN('.implode(',', $ids).')');
-            }
-            
-            foreach ($rep as $sum){
-                $total += $sum->prix;
-            }
-            return $total;
-                        
-        }
-
-        public function add($produit_id){
-            $_SESSION['panier'][$produit_id] = 1;
-        }
-
-        public function del($produit_id){
-            unset($_SESSION['panier'][$produit_id]);
-        }
-
-        public function count(){
-            return array_sum($_SESSION['panier']);
+        if (!in_array($id_produit, $_SESSION['panier'])) {
+            $_SESSION['panier'][] = $id_produit;
         }
     }
-    
-?> 
+
+    // Get cart contents from session
+    public function getPanier() {
+        return isset($_SESSION['panier']) ? $_SESSION['panier'] : [];
+    }
+
+    // Remove product from cart
+    public function removePanier($id_produit) {
+        if (isset($_SESSION['panier'])) {
+            $key = array_search($id_produit, $_SESSION['panier']);
+            if ($key !== false) {
+                unset($_SESSION['panier'][$key]);
+                $_SESSION['panier'] = array_values($_SESSION['panier']);
+            }
+        }
+    }
+
+    // Clear cart
+    public function clearPanier() {
+        unset($_SESSION['panier']);
+    }
+}
+?>
